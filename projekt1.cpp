4c55f98b6b7373f3e8027417166f55d4821cb411
Paweł Szczepankiewicz <pawszc@mat.umk.pl>
2024-12-28 14:27:10 +0100

    uzycie silni

--- projekt1.cpp
+++ projekt1.cpp
@@ -19,7 +19,11 @@ int main() {
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
-        cin >> wyjscie;
+        cout << "1. Silnia" << endl;
+	cin >> wyjscie;
+	if(wyjscie == 1){
+		cout << silnia(a) << endl;
+	}
     } while(wyjscie != 0);
     return 0;
 }
