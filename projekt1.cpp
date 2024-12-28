bcb7a2fb52b37353aea16d6a7464b74b3882ad23
Paweł Szczepankiewicz <pawszc@mat.umk.pl>
2024-12-28 14:31:16 +0100

    pierwsza

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+bool czyPierwsza(int liczba) {
+    if (liczba <= 1) return false; 
+    if (liczba <= 3) return true;  
+    if (liczba % 2 == 0 || liczba % 3 == 0) return false;
+    for (int i = 5; i * i <= liczba; i += 6) {
+        if (liczba % i == 0 || liczba % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
 
 int main() {
     int a, b;
